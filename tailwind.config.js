56d055a09e002e0f6c631bc21d02c1e79d160d65
lam-do <lamdo9491@gmail.com>
2021-07-25 20:40:07 +0700

    update safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       'components/**/*.{js,ts,jsx,tsx}'
     ],
     options: {
-      safelist: ["bg-gray-200", "bg-green-200", "bg-yellow-200"],
+      safelist: ["bg-gray-200", "bg-green-200", "bg-yellow-200", "bg-red-200"],
     }
   },
   darkMode: false,
