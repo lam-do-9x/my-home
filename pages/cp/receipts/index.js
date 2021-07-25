1e10a832219d79cacc2bc0d8e5f1da0e19c5c485
lam-do <lamdo9491@gmail.com>
2021-07-25 20:34:33 +0700

    receipt manager

--- pages/cp/receipts/index.js
+++ pages/cp/receipts/index.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import Image from 'next/image';
 import Layout from "../../../components/Layout";
-import { Client } from "@notionhq/client";
+import { notion } from '../../../lib/notion';
+import ReceiptModal from "../../../components/ReceiptModal";
 
 export default function Receipts({results}) {
+    const [modal, setModal] = useState(false);
+    const [block, setBlock] = useState(null);
+
+    function openPage(block) {
+        setBlock(block);
+        setModal(true);
+    }
+
     return (
         <Layout>
             <div className="overflow-x-auto">
@@ -10,12 +20,13 @@ export default function Receipts({results}) {
                 <div className="overflow-hidden">
                     <div className="flex flex-wrap mx-10 w-full">
                         {results.filter((id, block) => id !== 0).map(block => (
-                            <div className="flex flex-col justify-center items-center border border-gray-200 shadow-md rounded w-1/5 m-5 cursor-pointer" key={block.id} >
+                            <div className="flex flex-col justify-center items-center border border-gray-200 shadow-md rounded w-1/5 m-5 cursor-pointer" key={block.id} onClick={() => openPage(block)}>
                                 <Image className="object-fit" src={block.properties.cover ?? '/ngo-thanh-tung-pCTuLkx8erE-unsplash.jpg'} width={300} height={300} />
                                 <div className="pt-2 font-medium uppercase">{block.properties.name.title[0].plain_text}</div>
                                 <div className={`mb-2 rounded p-2 bg-${block.properties.type.select.color}-200`}>{block.properties.type.select.name}</div>
                             </div>
                         ))}
+                        {modal && <ReceiptModal block={block} onClick={() => setModal(false)}/>}
                     </div>
                 </div>
             </div>
@@ -24,11 +35,8 @@ export default function Receipts({results}) {
 }
 
 export async function getStaticProps() {
-    const notion = new Client({ auth: process.env.NOTION_API_OFFICIAL_KEYS });
-    const databaseId = process.env.NOTION_DATABASE_ID;
-    const response = await notion.databases.query({
-        database_id: databaseId,
-    });
+    const database_id = process.env.NOTION_DATABASE_ID;
+    const response = await notion.databases.query({ database_id });
 
     return {
         props: {
