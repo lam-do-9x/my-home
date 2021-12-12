33c32b3b1f815b4e022125b7f750511c14fcfc9b
leo-do-9x <leo.do.9x@gmail.com>
2021-12-12 10:09:06 +0700

    use class for get all child block

--- pages/api/receipts/[id].js
+++ pages/api/receipts/[id].js
@@ -1,7 +1,7 @@
-import { getAllChildBlockNotion } from "../../../lib/notion";
+import { AllChildBlockNotion } from "../../../lib/notion";
 
 export default async function handle(req, res) {
   const { id } = req.query;
-  const blocks = await getAllChildBlockNotion(id);
+  const blocks = await new AllChildBlockNotion(id).get();
   return res.json({ blocks, code: 200 });
 }
