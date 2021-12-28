6869db19a7ed589a732d8526be435967191b51e1
leo-do-9x <leo.do.9x@gmail.com>
2021-12-28 13:54:47 +0700

    upgrade tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,6 @@
 module.exports = {
-  mode: "jit",
-  purge: {
-    content: ["pages/**/*.{js,ts,jsx,tsx}", "components/**/*.{js,ts,jsx,tsx}"],
-    options: {
-      safelist: ["bg-gray-200", "bg-green-200", "bg-yellow-200", "bg-red-200"],
-    },
-  },
-  darkMode: false,
+  content: ["pages/**/*.{js,ts,jsx,tsx}", "components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
