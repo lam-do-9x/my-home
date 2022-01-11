ead1128afc584ecc61fe5082268702cd26724b0f
leo-do-9x <leo.do.9x@gmail.com>
2022-01-11 18:26:42 +0700

    border color 300

--- pages/cp/english/dictionary.js
+++ pages/cp/english/dictionary.js
@@ -201,11 +201,11 @@ function Dictionary() {
                 renderOnZeroPageCount={null}
                 className="py-4 flex items-center justify-center"
                 pageClassName="hover:bg-gray-100"
-                pageLinkClassName="inline-flex items-center px-4 py-2 border border-gray-100 text-gray-700 text-sm font-medium"
-                breakLinkClassName="inline-flex items-center px-4 py-2 border border-gray-100 text-gray-700 text-sm font-medium"
+                pageLinkClassName="inline-flex items-center px-4 py-2 border border-gray-300 text-gray-700 text-sm font-medium"
+                breakLinkClassName="inline-flex items-center px-4 py-2 border border-gray-300 text-gray-700 text-sm font-medium"
                 activeLinkClassName="bg-indigo-50 border-indigo-500 text-indigo-600"
-                previousLinkClassName="inline-flex items-center px-3 py-2 text-gray-700 rounded-l-md border border-gray-100"
-                nextLinkClassName="inline-flex items-center px-3 py-2 text-gray-700 rounded-r-md border border-gray-100"
+                previousLinkClassName="inline-flex items-center px-3 py-2 text-gray-700 rounded-l-md border border-gray-300"
+                nextLinkClassName="inline-flex items-center px-3 py-2 text-gray-700 rounded-r-md border border-gray-300"
                 disabledLinkClassName="text-gray-100 cursor-not-allowed"
               />
             </div>
