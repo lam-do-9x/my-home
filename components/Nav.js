f264fe3458dfc1313a6952333e9cdc45d78ebd75
leo-do-9x <leo.do.9x@gmail.com>
2022-08-01 17:05:44 +0700

    refactor nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,42 +1,84 @@
+import { useEffect, useRef } from 'react'
 import Link from 'next/link'
 
+const NAV = [
+  {
+    href: '/',
+    label: 'Lam Do',
+  },
+  [
+    {
+      href: '/blogs',
+      label: 'Blogs',
+    },
+  ],
+]
+
+function RedirectLink({ href, label, className }) {
+  return (
+    <Link key={label} href={href}>
+      <p className={`cursor-pointer ${className}`}>{label}</p>
+    </Link>
+  )
+}
+
+function NavList(nav) {
+  if (Array.isArray(nav)) {
+    return (
+      <ul className="flex" key={nav}>
+        {nav.map((value) => (
+          <li key={value.label} className="mx-2">
+            <RedirectLink href={value.href} label={value.label} />
+          </li>
+        ))}
+      </ul>
+    )
+  }
+  return (
+    <RedirectLink
+      key={nav.label}
+      href={nav.href}
+      label={nav.label}
+      className="text-4xl font-bold uppercase"
+    />
+  )
+}
+
 export default function Nav({ page = {} }) {
+  const isPage = Object.keys(page).length > 0
+  const navRef = useRef()
+
+  useEffect(() => {
+    if (isPage) {
+      const handleScroll = () => {
+        const hasOverScreenHeight = window.scrollY > window.screen.availHeight
+
+        navRef.current.classList.toggle('absolute', !hasOverScreenHeight)
+        navRef.current.classList.toggle('bg-transparent', !hasOverScreenHeight)
+        navRef.current.classList.toggle('text-white', !hasOverScreenHeight)
+        navRef.current.classList.toggle('fixed', hasOverScreenHeight)
+        navRef.current.classList.toggle('bg-white', hasOverScreenHeight)
+        navRef.current.classList.toggle('shadow', hasOverScreenHeight)
+      }
+
+      window.addEventListener('scroll', handleScroll)
+
+      return () => {
+        window.removeEventListener('scroll', handleScroll)
+      }
+    }
+  }, [])
+
   return (
     <nav
       className={`${
-        Object.keys(page).length !== 0 ? 'absolute z-30' : 'shadow'
-      } w-full py-4 px-6`}
+        isPage
+          ? 'absolute top-0 z-10 w-full bg-transparent text-white'
+          : 'shadow'
+      } flex items-center justify-between py-4 px-20 `}
+      ref={navRef}
     >
-      <div className="container mx-auto flex items-center justify-between">
-        <Link href="/">
-          <div
-            className={`${
-              Object.keys(page).length !== 0 ? 'text-white' : ''
-            } cursor-pointer text-4xl font-bold uppercase`}
-          >
-            Lam Do
-          </div>
-        </Link>
-        <div className="flex items-center justify-between">
-          <Link href="/blogs">
-            <p
-              className={`my-1 mx-4 ${
-                Object.keys(page).length !== 0 ? 'text-white' : ''
-              } cursor-pointer`}
-            >
-              Blogs
-            </p>
-          </Link>
-          <a
-            href="#"
-            className={`my-1 mx-4 ${
-              Object.keys(page).length !== 0 ? 'text-white' : ''
-            }`}
-          >
-            About me
-          </a>
-        </div>
-      </div>
+      {NAV.map((nav) => NavList(nav))}
     </nav>
   )
 }
