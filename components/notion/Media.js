d534a3831a980e0aecca36e197868a0286b6a166
leo-do-9x <leo.do.9x@gmail.com>
2022-08-02 11:38:52 +0700

    generate link preview

--- components/notion/Media.js
+++ components/notion/Media.js
@@ -1,3 +1,5 @@
+import Images from '@components/Images'
+
 export const Bookmarks = ({ value }) => {
   return (
     <a
@@ -6,20 +8,37 @@ export const Bookmarks = ({ value }) => {
       target="_blank"
       rel="noreferrer"
     >
-      <div className="border border-gray-300 p-8">{value.url}</div>
+      <div className="flex w-full justify-between border border-gray-300">
+        <div className="w-2/3 p-4">
+          <p className="my-0 text-base font-normal">{value.title}</p>
+          <p className="my-2 text-sm font-thin text-gray-700">
+            {value.description}
+          </p>
+          <div className="flex">
+            <Images src={value.favicons[0]} width="32" height="32" />
+            <p className="my-0 mx-2 text-base font-normal">{value.url}</p>
+          </div>
+        </div>
+        <Images
+          src={value.images[0]}
+          width="250"
+          height="106"
+          className="w-1/3"
+        />
+      </div>
     </a>
-  );
-};
+  )
+}
 
 export const Video = ({ value }) => {
   const regExp =
-    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-  const id = value[value.type].url.match(regExp);
+    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/
+  const id = value[value.type].url.match(regExp)
   return (
     <iframe
       src={`https://youtube.com/embed/${id[7]}`}
       width="100%"
       height="400"
     ></iframe>
-  );
-};
+  )
+}
--- pages/api/blogs/[id].js
+++ pages/api/blogs/[id].js
@@ -1,8 +1,15 @@
 import { pageNotion, AllChildBlockNotion } from '@lib/notion'
+import { getLinkPreview } from 'link-preview-js'
 
 export default async function handle(req, res) {
   const { id } = req.query
   const blocks = await new AllChildBlockNotion(id).get()
+  const lastItemIndex = blocks.length - 1
+  const linkPreview = await getLinkPreview(blocks[lastItemIndex].bookmark.url)
+  blocks[lastItemIndex] = {
+    type: 'bookmark',
+    bookmark: linkPreview,
+  }
   const page = await pageNotion(id)
   return res.json({ blocks, page, code: 200 })
 }
