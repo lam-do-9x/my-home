53d31e4d8879c51d1e4663d16c7dc0df4db47cc6
leo-do-9x <leo.do.9x@gmail.com>
2022-08-08 16:23:19 +0700

    remove next auth

--- middleware.js
+++ middleware.js
@@ -1,15 +1,15 @@
 import { NextResponse } from 'next/server'
 
-export async function middleware(request) {
-  const session = request.cookies.get('next-auth.session-token')
+export async function middleware() {
+  //   const session = request.cookies.get('next-auth.session-token')
 
-  const pathName = request.nextUrl.pathname
+  //   const pathName = request.nextUrl.pathname
 
-  if (!session && pathName.includes('/cp')) {
-    return NextResponse.redirect(
-      new URL(`/login?redirect=${pathName}`, request.url)
-    )
-  }
+  //   if (!session && pathName.includes('/cp')) {
+  //     return NextResponse.redirect(
+  //       new URL(`/login?redirect=${pathName}`, request.url)
+  //     )
+  //   }
 
   return NextResponse.next()
 }
