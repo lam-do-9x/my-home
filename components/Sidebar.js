2678d332da99ffc2ce5d8ff2eea7712ea2f1eecf
leo-do-9x <leo.do.9x@gmail.com>
2022-08-10 17:25:35 +0700

    refactor sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useRouter } from 'next/router'
 import {
   GlobeIcon,
@@ -14,34 +14,158 @@ import {
   TemplateIcon,
   LogoutIcon,
 } from '@heroicons/react/outline'
-import ActiveLink from './cp/ActiveLink'
+import ActiveLink from './ActiveLink'
+import { getContentByField, getStringBeforeCharacter } from '@lib/string'
+
+const SIDEBAR = [
+  [
+    {
+      icon: <GlobeIcon className="h-6 w-6" />,
+      label: 'English',
+    },
+    {
+      icon: <BookmarkAltIcon className="h-6 w-6" />,
+      label: 'Dictionary',
+      href: '/cp/english/dictionary',
+    },
+    {
+      icon: <FilmIcon className="h-6 w-6" />,
+      label: 'Youglish',
+      href: '/cp/english/youglish',
+    },
+    {
+      icon: <VolumeUpIcon className="h-6 w-6" />,
+      label: 'Pronunciation',
+      href: '/cp/english/pronunciation',
+    },
+  ],
+  [
+    {
+      icon: <ChatAlt2Icon className="h-6 w-6" />,
+      label: 'Communication',
+    },
+    {
+      icon: <UserIcon className="h-6 w-6" />,
+      label: 'Body Language',
+      href: '/cp/communication/body-language',
+    },
+    {
+      icon: <ChatIcon className="h-6 w-6" />,
+      label: 'Improvisation',
+      href: '/cp/communication/improvisation',
+    },
+  ],
+  {
+    icon: <ReceiptTaxIcon className="h-6 w-6" />,
+    label: 'Receipts',
+    href: '/cp/receipts',
+  },
+  {
+    icon: <TemplateIcon className="h-6 w-6" />,
+    label: 'Fashion',
+    href: '/cp/fashion',
+  },
+]
+
+function ActiveLinkEl({ href, icon, label }) {
+  return (
+    <ActiveLink activeClassName="bg-gray-200" href={href}>
+      <p className="flex cursor-pointer items-center justify-center rounded-lg p-3 leading-6 text-gray-900 hover:bg-gray-300">
+        {icon}
+        {label}
+      </p>
+    </ActiveLink>
+  )
+}
+
+function handleDropdown(e) {
+  const target = e.target.closest('div')
+  handleDropDownList(target)
+}
+
+function handleDropDownList(target) {
+  const dropdownList = target.parentElement.querySelector(
+    `#${target.dataset.label}`
+  )
+  const chevronUpIcon = target.querySelector('.up')
+  const chevronDownIcon = target.querySelector('.down')
+
+  dropdownList.classList.toggle('hidden')
+  chevronDownIcon.classList.toggle('hidden')
+  chevronUpIcon.classList.toggle('hidden')
+}
+
+function SidebarGroup(sidebar) {
+  return (
+    <>
+      <div
+        className="flex w-full items-center justify-center rounded-lg p-3 leading-6 hover:bg-gray-300"
+        data-label={sidebar[0].label.toLowerCase()}
+        onClick={handleDropdown}
+      >
+        {sidebar[0].icon}
+        <span className="mx-2 text-gray-900">{sidebar[0].label}</span>
+        <ChevronDownIcon className="down h-4 w-4" />
+        <ChevronUpIcon className="up hidden h-4 w-4" />
+      </div>
+      <div className="mt-2 hidden" id={sidebar[0].label.toLowerCase()}>
+        {sidebar.map((value, i) =>
+          i !== 0 ? (
+            <ActiveLinkEl
+              key={value.label}
+              href={value.href}
+              label={value.label}
+              icon={value.icon}
+            />
+          ) : null
+        )}
+      </div>
+    </>
+  )
+}
+
+function SideBarList(sidebar) {
+  if (Array.isArray(sidebar)) {
+    return (
+      <div
+        className="-mx-3 flex cursor-pointer flex-col text-sm font-medium"
+        key={sidebar}
+      >
+        {SidebarGroup(sidebar)}
+      </div>
+    )
+  }
+
+  return (
+    <ActiveLinkEl
+      key={sidebar.label}
+      href={sidebar.href}
+      label={sidebar.label}
+      icon={sidebar.icon}
+    />
+  )
+}
 
 export default function Sidebar() {
   const { pathname, push } = useRouter()
-  const [show, setShow] = useState(false)
-  const [section, setSection] = useState(undefined)
-  const pathShow = {
-    '/cp/english/youglish': 'english',
-    '/cp/english/dictionary': 'english',
-    '/cp/english/pronunciation': 'english',
-    '/cp/communication/body-language': 'communication',
-    '/cp/communication/improvisation': 'communication',
-  }
+  const sidebarRef = useRef()
 
   useEffect(() => {
-    if (pathShow[pathname]) {
-      setShow(true)
-      setSection(pathShow[pathname])
+    let label = getContentByField(pathname, '/cp/', '/')
+    if (label.includes('/')) {
+      label = getStringBeforeCharacter(label, '/')
+    }
+    if (label) {
+      const target = sidebarRef.current.querySelector(
+        `div[data-label=${label}]`
+      )
+      handleDropDownList(target)
+      const dropdownList = target.parentElement.querySelector(
+        `#${target.dataset.label}`
+      )
+      console.log(dropdownList)
     }
   }, [])
-
-  function dropdown(currentSection) {
-    setShow(true)
-    if (show === true && section === currentSection) {
-      setShow(false)
-    }
-    setSection(currentSection)
-  }
 
   async function handleLogout() {
     await fetch('/api/auth/logout')
@@ -49,129 +173,15 @@ export default function Sidebar() {
   }
 
   return (
-    <div className="w-64 bg-stone-50 p-6">
-      <div className="my-2 flex items-center justify-center text-xl font-bold uppercase text-gray-400 underline underline-offset-4">
+    <div className="w-64 bg-stone-50 p-6" ref={sidebarRef}>
+      <div className="my-2 flex items-center justify-center text-xl font-bold uppercase text-gray-700 underline underline-offset-4">
         Lam Do
         <LogoutIcon
           className="mx-2 h-6 w-6 cursor-pointer"
           onClick={handleLogout}
         />
       </div>
-      <div
-        className={`-mx-3 flex cursor-pointer flex-col rounded-lg p-3 text-sm font-medium ${
-          show && section === 'english' ? '' : 'hover:bg-gray-300'
-        }`}
-      >
-        <div className="flex flex-col items-center justify-center rounded-lg">
-          <div
-            className="flex w-full items-center justify-center"
-            onClick={() => dropdown('english')}
-          >
-            <GlobeIcon className="h-6 w-6" />
-            <span className="mx-4 text-gray-900">English</span>
-            {show && section === 'english' ? (
-              <ChevronUpIcon className="h-4 w-4" />
-            ) : (
-              <ChevronDownIcon className="h-4 w-4" />
-            )}
-          </div>
-          <div
-            className={`mt-2 w-full ${
-              show && section === 'english' ? '' : 'hidden'
-            }`}
-          >
-            <ActiveLink
-              activeClassName="bg-gray-200"
-              href="/cp/english/dictionary"
-            >
-              <p className="flex items-center justify-center rounded-lg p-3 leading-6 text-gray-900 hover:bg-gray-100">
-                <BookmarkAltIcon className="mr-2 h-6 w-6" />
-                Dictionary
-              </p>
-            </ActiveLink>
-            <ActiveLink
-              activeClassName="bg-gray-100"
-              href="/cp/english/youglish"
-            >
-              <p className="flex items-center justify-center rounded-lg p-3 leading-6 text-gray-900 hover:bg-gray-100">
-                <FilmIcon className="mr-2 h-6 w-6" />
-                Youglish
-              </p>
-            </ActiveLink>
-            <ActiveLink
-              activeClassName="bg-gray-100"
-              href="/cp/english/pronunciation"
-            >
-              <p className="flex items-center justify-center rounded-lg p-3 leading-6 text-gray-900 hover:bg-gray-100">
-                <VolumeUpIcon className="mr-2 h-6 w-6" />
-                Pronunciation
-              </p>
-            </ActiveLink>
-          </div>
-        </div>
-      </div>
-      <ActiveLink activeClassName="bg-gray-100" href="/cp/receipts">
-        <div
-          className="-mx-3 flex cursor-pointer items-center justify-center rounded-lg p-3 text-sm font-medium hover:bg-gray-100"
-          onClick={() => setShow(false)}
-        >
-          <ReceiptTaxIcon className="h-6 w-6" />
-          <span className="ml-4 text-gray-900">Receipts</span>
-        </div>
-      </ActiveLink>
-      <ActiveLink activeClassName="bg-gray-100" href="/cp/fashion">
-        <div
-          className="-mx-3 flex cursor-pointer items-center justify-center rounded-lg p-3 text-sm font-medium hover:bg-gray-100"
-          onClick={() => setShow(false)}
-        >
-          <TemplateIcon className="h-6 w-6" />
-          <span className="ml-4 text-gray-900">Fashion</span>
-        </div>
-      </ActiveLink>
-      <div
-        className={`-mx-3 flex cursor-pointer flex-col rounded-lg p-3 text-sm font-medium ${
-          show && section === 'communication' ? '' : 'hover:bg-gray-100'
-        }`}
-      >
-        <div className="flex flex-col items-center justify-center rounded-lg">
-          <div
-            className="flex w-full items-center justify-center"
-            onClick={() => dropdown('communication')}
-          >
-            <ChatAlt2Icon className="h-6 w-6" />
-            <span className="mx-4 text-gray-900">Communication</span>
-            {show && section === 'communication' ? (
-              <ChevronUpIcon className="h-4 w-4" />
-            ) : (
-              <ChevronDownIcon className="h-4 w-4" />
-            )}
-          </div>
-          <div
-            className={`mt-2 w-full ${
-              show && section === 'communication' ? '' : 'hidden'
-            }`}
-          >
-            <ActiveLink
-              activeClassName="bg-gray-100"
-              href="/cp/communication/body-language"
-            >
-              <p className="flex items-center justify-center rounded-lg p-3 leading-6 text-gray-900 hover:bg-gray-100">
-                <UserIcon className="h-6 w-6" />
-                Body Language
-              </p>
-            </ActiveLink>
-            <ActiveLink
-              activeClassName="bg-gray-100"
-              href="/cp/communication/improvisation"
-            >
-              <p className="flex items-center justify-center rounded-lg p-3 leading-6 text-gray-900 hover:bg-gray-100">
-                <ChatIcon className="mr-2 h-6 w-6" />
-                Improvisation
-              </p>
-            </ActiveLink>
-          </div>
-        </div>
-      </div>
+      {SIDEBAR.map((sidebar) => SideBarList(sidebar))}
     </div>
   )
 }
