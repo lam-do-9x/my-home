0e0209b795648c0ab18139a433a058c48eec9f61
leo-do-9x <leo.do.9x@gmail.com>
2023-07-01 15:47:21 +0700

    add sentence for detail of a dictionary

--- components/cp/Modal.js
+++ components/cp/Modal.js
@@ -1,8 +1,26 @@
+import { useEffect, useState } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import MDRender from './MDR'
 import styles from './CP.module.css'
+import AsyncMultiSelect from '@components/AsyncMultiSelect'
+import Loader from '@components/cp/Loader'
 
 export default function Modal(props) {
+    const [sentences, setSentences] = useState([])
+    const [isLoading, setLoading] = useState(true)
+
+    useEffect(async () => {
+            const url = `/api/dictionaries/${props.dictionary.id}/sentences`
+
+            const { sentences } = await fetch(url).then((response) => response.json())
+
+            setSentences(sentences)
+
+            setLoading(false)
+
+            return;
+    }, [])
+
   function close() {
     props.onClick()
   }
@@ -25,6 +43,24 @@ export default function Modal(props) {
         </div>
         <div className="prose my-2 overflow-x-hidden px-7">
           <MDRender content={props.dictionary.content} />
+        </div>
+        <div className="mb-4 w-full px-7">
+            {sentences.length > 0 ?< p className="mb-2 text-xl font-semibold">Sentences</p> : null}
+            <ul className="flex flex-wrap">
+                  {isLoading ? (
+                    <div className="border-b border-gray-200">
+                        <div colSpan="4">
+                        <Loader />
+                        </div>
+                    </div>
+                    ) : (
+                        sentences?.map((sentence) => (
+                            <li key={sentence.value} className="my-2 mr-2 border p-2">
+                                {sentence.label}
+                            </li>
+                        ))
+                    )}
+            </ul>
         </div>
       </div>
     </div>
